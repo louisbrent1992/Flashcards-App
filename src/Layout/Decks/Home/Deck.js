bf04841507d4e3d19f30a598973674d802632b43
Louis Brent <louisbrent1992@gmail.com>
2022-01-11 10:58:08 -0800

    updated deck with button icons

--- src/Layout/Decks/Home/Deck.js
+++ src/Layout/Decks/Home/Deck.js
@@ -1,46 +1,47 @@
-import React from "react"
-import { Link, useHistory } from "react-router-dom"
+import React from "react";
+import { Link, useHistory } from "react-router-dom";
 
-import { deleteDeck } from "../../../utils/api"
+import { deleteDeck } from "../../../utils/api";
 
-const Deck = ( { deck } ) => {
-  
+const Deck = ({ deck }) => {
   // Hook to send user home
   const goBack = useHistory();
 
   // Delete handler: deletes deck and reloads DeckList, or sends user home
   const handleDelete = async () => {
-    const message = "Delete this deck?"
-    const confirm = window.confirm(message)
-    confirm === true ? 
-    await deleteDeck(deck.id) : goBack.push("/");
-  }
+    const message = "Delete this deck?";
+    const confirm = window.confirm(message);
+    confirm === true ? await deleteDeck(deck.id) : goBack.push("/");
+  };
 
   return (
-    <div className="rounded container border border-secondary mt-1 py-1 by-1">
-      <div className="row my-1">
-        <div className="col text-left">
-          <h5>{deck.name}</h5>
-        </div>
-        <div className="col text-right">
-          <p className="text-right">{deck.cards.length} cards</p>
-        </div>
+    <div className="rounded container border border-secondary mt-2 py-1 by-1">
+      <div className="row mx-2 d-flex justify-content-between">
+        <h5>{deck.name}</h5>
+        <p>{deck.cards.length} cards</p>
+        <p>{deck.description}</p>
       </div>
-      <p>{deck.description}</p>
-      <div className="row my-1">
-        <div className="col">
-          <Link to={ "/decks/" + deck.id } className="btn btn-secondary">View</Link>
+      <div className="row mx-2 mb-2 d-flex justify-content-between ">
+        <div className="d-flex justify-content-between">
+          <Link to={"/decks/" + deck.id} className="btn btn-secondary mr-2">
+            <i className="bi bi-eye"> View</i>
+          </Link>
+          <Link to={"/decks/" + deck.id + "/study"} className="btn btn-primary">
+            <i className="bi bi-journal-bookmark-fill"> Study</i>
+          </Link>
         </div>
-        <div className = "col">
-          <Link to={ "/decks/" + deck.id + "/study" } className="btn btn-primary">Study</Link>
-        </div>
-        <div className = "col">
-          <button type="button" className="btn btn-danger" onClick={handleDelete}>Delete</button>
+        <div>
+          <button
+            type="button"
+            className="btn btn-danger"
+            onClick={handleDelete}
+          >
+            <i className="bi bi-trash" />
+          </button>
         </div>
       </div>
     </div>
-  )
+  );
+};
 
-}
-
-export default Deck
+export default Deck;
