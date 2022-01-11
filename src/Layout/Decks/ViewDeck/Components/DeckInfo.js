122c45a60d32b28e04c9c2ae5e2f75b5cb4aca9a
Louis Brent <louisbrent1992@gmail.com>
2022-01-11 13:54:30 -0800

    updated deckinfo view

--- src/Layout/Decks/ViewDeck/Components/DeckInfo.js
+++ src/Layout/Decks/ViewDeck/Components/DeckInfo.js
@@ -1,24 +1,24 @@
-import React from "react"
-import { Link, useHistory, useRouteMatch } from "react-router-dom"
+import React from "react";
+import { Link, useHistory, useRouteMatch } from "react-router-dom";
 
-import { deleteDeck } from "../../../../utils/api"
+import { deleteDeck } from "../../../../utils/api";
 
-const DeckInfo = ( { deck } ) => {
-
+const DeckInfo = ({ deck }) => {
   // Hooks to send user elsewhere or keep them here
   const goBack = useHistory();
-  const url = useRouteMatch().url
+  const url = useRouteMatch().url;
 
   // Dialog box: deletes deck and sends user home, or keeps deck and keeps user here
   const handleDelete = async () => {
-    const message = "Delete this deck?"
+    const message = "Delete this deck?";
     const confirm = window.confirm(message);
-    confirm === true ? 
-    await deleteDeck(deck.id) && goBack.push("/") : goBack.push(url)
-  }
+    confirm === true
+      ? (await deleteDeck(deck.id)) && goBack.push("/")
+      : goBack.push(url);
+  };
 
   return (
-    <div className="rounded border border-secondary p-2">
+    <div className="rounded container border border-secondary mt-2 py-1 by-1">
       <div className="row">
         <div className="col">
           <h3>{deck.name}</h3>
@@ -26,27 +26,29 @@ const DeckInfo = ( { deck } ) => {
         </div>
       </div>
       {/* Action buttons: edit, study, add card, delete */}
-      <div className="row">
-        <div className="col">
-          {/* Edit button: sends user to deck form */}
-          <Link to={"/decks/" + deck.id + "/edit"} className="btn btn-secondary">Edit</Link>
-        </div>
-        <div className="col">
-          {/* Study button: sends user to study screen */}
-          <Link to={"/decks/" + deck.id + "/study"} className="btn btn-primary">Study</Link>
-        </div>
-        <div className="col">
-          {/* Add card: sends user to card form */}
-          <Link to={"/decks/" + deck.id + "/cards/new"} className="btn btn-primary">Add Card</Link>
-        </div>
-        <div className="col">
-          {/* Delete button */}
-          <button type="button" className="btn btn-danger" onClick={handleDelete}>Delete</button>
-        </div>
+      <div className="row mx-1 mb-2 d-flex justify-content-between">
+        {/* Edit button: sends user to deck form */}
+        <Link to={"/decks/" + deck.id + "/edit"} className="btn btn-secondary">
+        <i class="bi bi-pen"> Edit</i>
+        </Link>
+        {/* Study button: sends user to study screen */}
+        <Link to={"/decks/" + deck.id + "/study"} className="btn btn-primary">
+        <i className="bi bi-journal-bookmark-fill">  Study</i>
+        </Link>
+        {/* Add card: sends user to card form */}
+        <Link
+          to={"/decks/" + deck.id + "/cards/new"}
+          className="btn btn-primary"
+        >
+          <i class="bi bi-plus-lg"> Add Card</i>
+        </Link>
+        {/* Delete button */}
+        <button type="button" className="btn btn-danger" onClick={handleDelete}>
+        <i className="bi bi-trash" />
+        </button>
       </div>
-    </div>    
-  )
+    </div>
+  );
+};
 
-}
-
-export default DeckInfo
+export default DeckInfo;
